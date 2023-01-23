c3d5506467318def003844d38e62cf07c73a4048
mendsalbert <mendsalbert@gmail.com>
2023-01-23 19:00:18 +0000

    screens

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { RestaurantScreen } from "./src/features/restaurant/screens/restaurants.
 import { theme } from "./src/infrastructure/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import {
   useFonts as oswaldloaded,
   Oswald_400Regular,
@@ -25,13 +26,42 @@ export default function App() {
     return null;
   }
 
+  //fake screens
+  const Maps = () => {
+    return <Text>Maps</Text>;
+  };
+
+  const Settings = () => {
+    return <Text>Settings</Text>;
+  };
+
   const Tab = createBottomTabNavigator();
 
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Restaurant") {
+                iconName = focused
+                  ? "ios-restaurant-sharp"
+                  : "ios-restaurant-outline";
+              } else if (route.name === "Maps") {
+                iconName = focused ? "map" : "map";
+              }
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: "tomato",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
           <Tab.Screen name="Restaurant" component={RestaurantScreen} />
+          <Tab.Screen name="Maps" component={Maps} />
+          <Tab.Screen name="Settings" component={Settings} />
           {/* <RestaurantScreen /> */}
         </Tab.Navigator>
       </NavigationContainer>
