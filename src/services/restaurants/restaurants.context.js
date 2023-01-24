8938c3077b68d0c0e9ea696c27fed3c245d3b074
mendsalbert <mendsalbert@gmail.com>
2023-01-24 06:43:31 +0000

    context

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,1 +1,15 @@
-import React from "react";
+import React, {
+  useEffect,
+  useState,
+  useContext,
+  useMemo,
+  createContext,
+} from "react";
+
+import { restaurantRequest, restaurantsTransform } from "./restaurants.service";
+
+export const RestaurantsContext = createContext();
+
+export const RestaurantsContextProvider = ({ children }) => {
+  return <RestaurantsContext.Provider>{children}</RestaurantsContext.Provider>;
+};
