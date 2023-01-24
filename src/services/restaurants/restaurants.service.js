1705f2cb6c0cc6822f701962dbc144493d08acbf
mendsalbert <mendsalbert@gmail.com>
2023-01-24 06:08:02 +0000

    context

---
+++ src/services/restaurants/restaurants.service.js
@@ -0,0 +1,24 @@
+import camelize from "camelize";
+import { mocks } from "./mock";
+
+export const restaurantRequest = (location = "-30,32") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject("not found");
+    }
+    resolve(mock);
+  });
+};
+
+const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
+  });
+
+  return camelize(mappedResults);
+};
