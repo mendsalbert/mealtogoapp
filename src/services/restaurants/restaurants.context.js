5ee7e8e8819f6022df98d4acda1508346eed1b9d
mendsalbert <mendsalbert@gmail.com>
2023-01-24 08:02:54 +0000

    fixes

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -11,8 +11,31 @@ import { restaurantRequest, restaurantsTransform } from "./restaurants.service";
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const retrieveRestaurants = () => {
+    setTimeout(() => {
+      setIsLoading(true);
+      restaurantRequest()
+        .then(restaurantsTransform)
+        .then((results) => {
+          setRestaurants(results);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setError(err);
+        });
+    }, 2000);
+  };
+
+  useEffect(() => {
+    retrieveRestaurants();
+  }, []);
+
   return (
-    <RestaurantsContext.Provider value={{ restaurants: [1, 2, 3] }}>
+    <RestaurantsContext.Provider value={{ restaurants, isLoading, error }}>
       {children}
     </RestaurantsContext.Provider>
   );
