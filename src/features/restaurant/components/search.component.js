683f83df444d969f006cdca8049bb0580e829138
mendsalbert <mendsalbert@gmail.com>
2023-01-30 23:32:42 +0000

    zurfe

---
+++ src/features/restaurant/components/search.component.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const SearchBar = styled(Searchbar)`
+  width: 100%;
+  padding: 0;
+  border-radius: 30px;
+  background-color: white;
+`;
+
+const SearchWrapper = styled.View`
+  background-color: white;
+  padding: 10px;
+`;
+
+export const Search = () => {
+  <SearchWrapper>
+    <SearchBar />
+  </SearchWrapper>;
+};
