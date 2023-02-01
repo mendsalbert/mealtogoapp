9aa463930fb31248699ec4173b83b092ccbdb4e3
mendsalbert <mendsalbert@gmail.com>
2023-02-01 23:32:09 +0000

    zurfe

--- src/features/restaurant/screens/restaurants.screen.js
+++ src/features/restaurant/screens/restaurants.screen.js
@@ -43,7 +43,11 @@ export const RestaurantScreen = ({ navigation }) => {
         renderItem={({ item }) => {
           return (
             <TouchableOpacity
-              onPress={() => navigation.navigate("RestaurantDetail")}
+              onPress={() =>
+                navigation.navigate("RestaurantDetail", {
+                  restaurant: item,
+                })
+              }
             >
               <RestaurantInfo restaurant={item} />
             </TouchableOpacity>
