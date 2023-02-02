fc8a155e5427aa46f85e522f01c05b644c9d0d29
mendsalbert <mendsalbert@gmail.com>
2023-02-02 12:11:17 +0000

    zurfe

---
+++ src/services/authentication/authentication.service.js
@@ -0,0 +1,21 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpYzTk-OOLwkzLpO4BIdGpMCRH88zfItQ",
+  authDomain: "mealtogo-776a4.firebaseapp.com",
+  projectId: "mealtogo-776a4",
+  storageBucket: "mealtogo-776a4.appspot.com",
+  messagingSenderId: "1079595158914",
+  appId: "1:1079595158914:web:96f6b19612046d63d6d39b",
+  measurementId: "G-LJYFZ4E9CT",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const loginRequest = (email, password) =>
+  signInWithEmailAndPassword(auth, email, password);
