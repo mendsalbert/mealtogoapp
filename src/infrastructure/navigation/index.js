c696986fa5c0a93092e49f0f1f27d9976a36028c
mendsalbert <mendsalbert@gmail.com>
2023-02-02 12:43:49 +0000

    zurfe

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,8 +1,13 @@
 import React, { useContext } from "react";
 import { AuthenticationContext } from "../../services/authentication/authentication.context";
 import { AppNavigator } from "./app.navigator";
-import 
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 export const Navigation = () => {
   const { isAuthenticated } = useContext(AuthenticationContext);
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <AppNavigator /> : <AccountNavigation />}
+    </NavigationContainer>
+  );
 };
