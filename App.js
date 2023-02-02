b649aab58a142748d2db3323baa26f3d87f33bbd
mendsalbert <mendsalbert@gmail.com>
2023-02-02 11:46:13 +0000

    zurfe

--- App.js
+++ App.js
@@ -3,6 +3,24 @@ import * as React from "react";
 import { useContext } from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyBpYzTk-OOLwkzLpO4BIdGpMCRH88zfItQ",
+  authDomain: "mealtogo-776a4.firebaseapp.com",
+  projectId: "mealtogo-776a4",
+  storageBucket: "mealtogo-776a4.appspot.com",
+  messagingSenderId: "1079595158914",
+  appId: "1:1079595158914:web:96f6b19612046d63d6d39b",
+  measurementId: "G-LJYFZ4E9CT",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 import {
   useFonts as oswaldloaded,
@@ -17,6 +35,23 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { Navigation } from "./src/infrastructure/navigation";
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  React.useEffect(() => {
+    signInWithEmailAndPassword(auth, "mendsalbert@gmail.com", "1234567")
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        setIsAuthenticated(true);
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  }, []);
+
   const [oswaldLoaded] = oswaldloaded({
     Oswald_400Regular,
   });
